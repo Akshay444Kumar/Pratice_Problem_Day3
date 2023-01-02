6269c6cae32a35d89653b01287ad0d4c6149fc21
Akshay444Kumar <aryakshay444@gmail.com>
2023-01-02 18:08:53 +0530

    Calculation of Daily Wage

--- src/Wagecalculation/WageCalculation.java
+++ src/Wagecalculation/WageCalculation.java
@@ -5,14 +5,19 @@ public class WageCalculation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         double emp_check = Math.floor(Math.random()*10)%2;
+        double wage_perhour = 20;
+        double full_day = 8;
         if (emp_check ==0)
         {
             System.out.println("Employee is Absent");
+            System.out.println("Daily Wage of the Employee = 0");
         }
         else
         {
             System.out.println("Employee is Present");
+            System.out.println("Daily Wage of the Employee = " + wage_perhour * full_day);
         }
+
     }
 
 }
