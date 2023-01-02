5f55a3ee473ca2151e61f4110a19d153cd370469
Akshay444Kumar <aryakshay444@gmail.com>
2023-01-02 17:56:31 +0530

    To Check Employee Attendance

--- src/Wagecalculation/WageCalculation.java
+++ src/Wagecalculation/WageCalculation.java
@@ -4,6 +4,15 @@ package Wagecalculation;
 public class WageCalculation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+        double emp_check = Math.floor(Math.random()*10)%2;
+        if (emp_check ==0)
+        {
+            System.out.println("Employee is Absent");
+        }
+        else
+        {
+            System.out.println("Employee is Present");
+        }
     }
 
 }
