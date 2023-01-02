fc8819f6847fcc3c221828485680991663daa8b2
Akshay444Kumar <aryakshay444@gmail.com>
2023-01-02 18:25:36 +0530

    Daily Wage Computation of Part-time Employee

--- src/Wagecalculation/WageCalculation.java
+++ src/Wagecalculation/WageCalculation.java
@@ -4,15 +4,19 @@ package Wagecalculation;
 public class WageCalculation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-        double emp_check = Math.floor(Math.random()*10)%2;
+        double emp_check = Math.floor(Math.random()*10)%3;
         double wage_perhour = 20;
         double full_day = 8;
+        double part_time = 4;
         if (emp_check ==0)
         {
             System.out.println("Employee is Absent");
             System.out.println("Daily Wage of the Employee = 0");
-        }
-        else
+        } else if (emp_check == 1) {
+            System.out.println("Employee is present but is working part-time");
+            System.out.println("Daily Wage of the Part-time Employee = " + part_time * wage_perhour);
+
+        } else
         {
             System.out.println("Employee is Present");
             System.out.println("Daily Wage of the Employee = " + wage_perhour * full_day);
