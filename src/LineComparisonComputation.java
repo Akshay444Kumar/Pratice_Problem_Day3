e552981e92b1f9c728392ba4c2c4c5bbe00a687e
Akshay444Kumar <aryakshay444@gmail.com>
2023-01-04 15:06:54 +0530

    Calculating Length of Line

--- src/LineComparisonComputation.java
+++ src/LineComparisonComputation.java
@@ -1,5 +1,14 @@
+import java.lang.Math;
 public class LineComparisonComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
+        double x1=3;
+        double y1=6;
+        double x2=8;
+        double y2=7;
+        // Calculate Length between two points
+        double length = Math.sqrt((Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2)));
+        System.out.println("Distance between two points :" + length);
     }
+
 }
