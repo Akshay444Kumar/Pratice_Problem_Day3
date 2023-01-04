0480ff55cf6b3e45372c01cf447ed53495f8b90c
Akshay444Kumar <aryakshay444@gmail.com>
2023-01-04 14:37:08 +0530

    Employee Wage Computation using Switch Case

---
+++ src/Wagecalculation/Oopsconcept.java
@@ -0,0 +1,4 @@
+package Wagecalculation;
+
+public class Oopsconcept {
+}
--- src/Wagecalculation/WageCalculation.java
+++ src/Wagecalculation/WageCalculation.java
@@ -4,23 +4,27 @@ package Wagecalculation;
 public class WageCalculation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-        double emp_check = Math.floor(Math.random()*10)%3;
+        int emp_check = (int) Math.floor(Math.random()*10)%3;
         double wage_perhour = 20;
         double full_day = 8;
         double part_time = 4;
-        if (emp_check ==0)
-        {
-            System.out.println("Employee is Absent");
-            System.out.println("Daily Wage of the Employee = 0");
-        } else if (emp_check == 1) {
-            System.out.println("Employee is present but is working part-time");
-            System.out.println("Daily Wage of the Part-time Employee = " + part_time * wage_perhour);
-
-        } else
-        {
-            System.out.println("Employee is Present");
-            System.out.println("Daily Wage of the Employee = " + wage_perhour * full_day);
+        switch (emp_check) {
+            case 0:
+                System.out.println("Employee is Present");
+                System.out.println("Daily Wage of the Employee = " + wage_perhour * full_day);
+                break;
+            case 1:
+                System.out.println("Employee is present but is working part-time");
+                System.out.println("Daily Wage of the Part-time Employee = " + part_time * wage_perhour);
+                break;
+            default:
+                System.out.println("Employee is Absent");
+                System.out.println("Daily Wage of the Employee = 0");
+                break;
         }
+
+
+
 
     }
 
